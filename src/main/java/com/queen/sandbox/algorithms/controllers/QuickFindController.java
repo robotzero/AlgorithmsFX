870c10b8ff8465386163c6f4b0d36eb12437cb88
Agnieszka K. Kocinski <0robotzero@gmail.com>
2017-02-08 20:50:49 +0000

    Fixed circle not showing.

--- src/main/java/com/queen/sandbox/algorithms/controllers/QuickFindController.java
+++ src/main/java/com/queen/sandbox/algorithms/controllers/QuickFindController.java
@@ -15,8 +15,6 @@ import javafx.scene.text.Text;
 import java.net.URL;
 import java.util.*;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 public class QuickFindController implements Initializable {
 
@@ -77,9 +75,9 @@ public class QuickFindController implements Initializable {
 
                 if (!(distance < Math.pow(radius, 2))) {
                     if (this.pressedRectangles.contains(person.getId())) {
-                        person.getCircle().setVisible(false);
+                        person.getCircle().setVisible(true);
                     }
-                    if (!person.isRectanglePressed()) {
+                    if (!this.pressedRectangles.contains(person.getId())) {
                         person.getCircle().setVisible(false);
                     }
                 }
@@ -89,17 +87,12 @@ public class QuickFindController implements Initializable {
 
             person.getRectangle().setOnMousePressed(e -> {
                 int numberOfPressedRectangles = this.pressedRectangles.size();
-//                long numberOfPressedRectangles = currentListOfPressedRectangles(dataContainer.entrySet().stream()).get();
                 if (this.pressedRectangles.contains(person.getId()) && numberOfPressedRectangles != 0) {
-//                if (person.isRectanglePressed() && numberOfPressedRectangles != 0) {
-                    //@TODO fix it!!!
-                    this.pressedRectangles.remove(person.getId());
-//                    person.setRectanglePressed(false);
+                    this.pressedRectangles.remove(new Integer(person.getId()));
                     person.getCircle().setVisible(false);
                 } else {
                     if (numberOfPressedRectangles != 2) {
                         this.pressedRectangles.add(person.getId());
-//                        person.setRectanglePressed(true);
                         this.pressedRectangles.stream()
                                 .filter(personId -> person.getId() != personId)
                                 .findFirst()
@@ -122,43 +115,12 @@ public class QuickFindController implements Initializable {
                                                     this.QFwindow
                                             );
                                             this.pressedRectangles.clear();
-//                                            this.quickFind.getDatContainer().forEach((p, id) -> {
-//                                                p.setRectanglePressed(false);
-//                                            });
                                         } else {
                                             if (numberOfPressedRectangles == 1) {
                                                 this.initialConnectionLine.setVisible(false);
                                             }
                                         }
                                     });
-//                                });
-//                        dataContainer.entrySet().stream().filter(entryset -> entryset.getKey().isRectanglePressed())
-//                                .filter(entryset -> entryset.getKey() != person)
-//                                .findFirst().ifPresent(toConnect -> {
-//                                    if (numberOfPressedRectangles == 1 && !this.quickFind.connected(person, toConnect.getKey())) {
-//                                        this.quickFind.union(person, toConnect.getKey());
-//                                        Rectangle rootRectangle = toConnect.getKey().getRectangle();
-//                                        Rectangle toAnimate = person.getRectangle();
-//                                        this.animationPlayer.play(
-//                                            rootRectangle,
-//                                            toAnimate
-//                                        );
-//                                        this.initialConnectionLine.setVisible(false);
-//                                        this.lineFactory.addNewConnectionLine(
-//                                            person.getCircle().centerXProperty(),
-//                                            person.getCircle().centerYProperty(),
-//                                            toConnect.getKey().getCircle().centerXProperty(),
-//                                            toConnect.getKey().getCircle().centerYProperty(),
-//                                            this.QFwindow
-//                                        );
-//                                        this.quickFind.getDatContainer().forEach((p, id) -> {
-//                                            p.setRectanglePressed(false);
-//                                        });
-//                                    } else {
-//                                        if (numberOfPressedRectangles == 1) {
-//                                            this.initialConnectionLine.setVisible(false);
-//                                        }
-//                                    }
                                 });
                             }
                         }
@@ -168,14 +130,10 @@ public class QuickFindController implements Initializable {
 
         this.QFwindow.setOnMouseMoved(e -> {
             int numberOfPressedRectangles = this.pressedRectangles.size();
-//            long numberOfPressedRectangles = currentListOfPressedRectangles(dataContainer.entrySet().stream()).get();
             if (numberOfPressedRectangles == 1) {
                 int pressedRectangleId = this.pressedRectangles.get(0);
                 Circle circle = Optional.of(dataContainer.entrySet().stream()
                         .filter(entryset -> entryset.getValue() == pressedRectangleId).findFirst().get().getKey().getCircle()).get();
-//                Circle circle = Optional.of(dataContainer.entrySet().stream()
-//                        .filter(entryset -> entryset.getKey().isRectanglePressed())
-//                        .findFirst().get()).get().getKey().getCircle();
 
                 if (!this.initialConnectionLine.isVisible()) {
                     this.setInitialConnectionLinePref(line -> {
@@ -212,9 +170,5 @@ public class QuickFindController implements Initializable {
 
     private void setInitialConnectionLinePref(Consumer<Line> newSetup) {
             newSetup.accept(this.initialConnectionLine);
-    }
-
-    private Supplier<Long> currentListOfPressedRectangles(Stream<Map.Entry<Person, Integer>> currentStream) {
-        return () -> currentStream.filter(entry -> entry.getKey().isRectanglePressed()).count();
     }
 }
